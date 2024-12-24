af5284e9dfacabb53a2be36b7ec8cb7a638d7f6c
rahul <rahulstar00786@gmail.com>
2024-12-24 11:09:56 +0530

    added devdebugin prduction

--- app/components/layout/BaseNavbar.tsx
+++ app/components/layout/BaseNavbar.tsx
@@ -15,7 +15,7 @@ import { DevDebug, LanguageSelector, ThemeSwitcher } from "@/app/components";
 
 const BaseNavbar = () => {
     const devEnv = useMemo(() => {
-        return process.env.NODE_ENV === "development";
+        return process.env.NODE_ENV === 'production';
     }, []);
 
     return (
